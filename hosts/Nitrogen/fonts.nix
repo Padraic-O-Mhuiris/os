2a62f77cd5c4367967f9a8a295deb3005fa25417
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-05-20 21:32:49 +0100

    trimmed down fonts

--- hosts/Nitrogen/fonts.nix
+++ hosts/Nitrogen/fonts.nix
@@ -2,20 +2,7 @@
   fonts = {
     fonts = with pkgs; [
       iosevka
-      nerdfonts
       roboto
-      emacs-all-the-icons-fonts
-      xorg.fontbhlucidatypewriter100dpi
-      xorg.fontbhlucidatypewriter75dpi
-      dejavu_fonts
-      fira-code
-      freefont_ttf
-      gyre-fonts # TrueType substitutes for standard PostScript fonts
-      liberation_ttf
-      xorg.fontbh100dpi
-      xorg.fontmiscmisc
-      xorg.fontcursormisc
-      unifont
     ];
     fontconfig = {
       enable = true;
